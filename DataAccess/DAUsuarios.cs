14aff899cf502445ccd036927b11ea179c08549c
Romina Merlo <rominamerlo07@gmail.com>
2021-09-28 17:28:39 -0300

    login

---
+++ DataAccess/DAUsuarios.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entidades.ent;
+
+namespace DataAccess
+{
+    public class DAUsuarios
+    {
+
+        SqlConnectionManager SqlConnectionManager = new SqlConnectionManager();
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataAdapter dta;
+        DataTable dt;
+        SqlDataReader dr;
+        SqlTransaction trans;
+
+        public bool accederUsuario(string NombreUsuario, string passwordUsuario)
+        {
+            try
+            {
+                string cadenaDeConexion = SqlConnectionManager.getCadenaConexion();
+                con = new SqlConnection(cadenaDeConexion);
+
+                string consulta = "SELECT * FROM T_USUARIOS " +
+                                   "WHERE NOMBRE_USUARIO = @USUARIO " +
+                                   "AND CLAVE_USUARIO = @PASSWORD " +
+                                   "AND FECHA_BAJA IS NULL; ";
+
+                cmd = new SqlCommand(consulta, con);
+
+                if (NombreUsuario != "" && passwordUsuario != "")
+                {
+                    cmd.Parameters.AddWithValue("@USUARIO", NombreUsuario);
+                    cmd.Parameters.AddWithValue("@PASSWORD", passwordUsuario);
+                }
+
+                else
+                {
+                    cmd.Parameters.AddWithValue("@USUARIO", DBNull.Value);
+                    cmd.Parameters.AddWithValue("@PASSWORD", DBNull.Value);
+                }
+
+                dta = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                dta.Fill(dt);
+
+                Usuario usuario = new Usuario();
+                if (dt.Rows.Count > 0)
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (dr["ID_USUARIO"] != DBNull.Value)
+                            usuario.IdUsuario = Convert.ToInt32(dr["ID_USUARIO"]);
+                        if (dr["NOMBRE_USUARIO"] != DBNull.Value)
+                            usuario.NombreUsuario = Convert.ToString(dr["NOMBRE_USUARIO"]);
+                        if (dr["CLAVE_USUARIO"] != DBNull.Value)
+                            usuario.ClaveUsuario = Convert.ToString(dr["CLAVE_USUARIO"]);                        
+                    }
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ TurneroWeb10/Login.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogicLayer.Gestores;
+using Entidades.ent;
+
+namespace TurneroWeb10
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static string accesoUser(string p_usuario, string p_password)
+        {
+            
+            Usuario usuario = new Usuario();
+            GestorUsuarios gestorUsuarios = new GestorUsuarios();
+
+            try
+            {
+               // string mensaje = "OK";
+
+  
+                bool result = gestorUsuarios.accederUsuario(p_usuario, p_password);
+
+                if (result)
+                {
+                    return "OK";
+                }
+                else {
+                    return "Error";
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                string error = "Se produjo un error al buscar los datos del usuario " + e.Message;
+                return error;
+            }
+
+        }
+    }
+}
