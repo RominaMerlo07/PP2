7f9543b6ca797d36157d1857e179065dbc4e6fe4
Asnotragico <asnotragico@gmail.com>
2022-02-11 09:33:00 -0300

    correccion de obtenerCentros()

--- TurneroWeb10/DisponibilidadHoraria.aspx.cs
+++ TurneroWeb10/DisponibilidadHoraria.aspx.cs
@@ -77,7 +77,7 @@ namespace TurneroWeb10
             try
             {
                 GestorCentros gestorCentros = new GestorCentros();
-                List<Centro> centros = gestorCentros.traerCentros();
+                List<Centro> centros = gestorCentros.obtenerCentros();
                 return centros;
                 //string col = JsonConvert.SerializeObject(centros);
                 //return col;
